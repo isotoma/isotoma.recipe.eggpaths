7ed2972448f1465c83ff92657899b77e67f1aab6
Tom Wardill <tom@howrandom.net>
2011-05-19 11:12:15 +0100

    Working path generator

--- isotoma/recipe/eggpaths/__init__.py
+++ isotoma/recipe/eggpaths/__init__.py
@@ -13,10 +13,16 @@
 # limitations under the License.
 
 import os
+import sys
+import logging
 
 from zc.buildout import easy_install
 
 class Eggpaths(object):
+    
+    def foo(self, dist):
+        print dist
+        
 
     def __init__(self, buildout, name, options):
         """ Take the eggs we're given, and save the path to them into buildout variables """ 
@@ -26,42 +32,30 @@ class Eggpaths(object):
         self.name = name
         self.options = options
 
+        # check if we have versions information
+        # if we don't, there's not a lot we can really do
+        if not self.buildout['buildout'].has_key('versions'):
+            # log and bail
+            logging.error('Buildout does not have versions information')
+            sys.exit(1)
+        
+        # get the versions section from buildout
+        versions_name = self.buildout['buildout']['versions']
+        versions_section = self.buildout[versions_name]
+        
+        # stick this in our local buildout space
+        # we also need to add the path to the egg
+        for k,v in versions_section.iteritems():
+            egg_directory = self.buildout['buildout']['eggs-directory']
+            python_version = '.'.join([str(x) for x in sys.version_info[0:2]])
+            
+            path_to_egg = os.path.join(egg_directory, k + '-' + v + '-py' + python_version +'.egg/')
+            
+            self.options.setdefault(k, path_to_egg)
+        
+
     def install(self):
         """ Write out the paths that we've found to a handy text file """
-        
-        # get the list of eggs that was specified in the buildout part
-        eggs = [x.strip() for x in self.options['eggs'].split('\n')]
-
-        # get the directories that the eggs will be installed in from buildout
-        egg_paths = [
-            self.buildout["buildout"]["develop-eggs-directory"],
-            self.buildout["buildout"]["eggs-directory"],
-            ]
-
-        # get the current working set of the eggs
-        ws = easy_install.working_set(eggs, self.buildout['buildout']['executable'] ,egg_paths)
-
-        # get the eggs we're interested in out of the working set
-        self.eggs_of_interest = {}
-        for egg in eggs:
-            # get a dict of the egg names that have actually been installed against the paths used
-            entry_names = {}
-            for entry in ws.entries:
-                entry_names[entry.split('/')[-1]] = entry
-
-            # now we have the eggs names and paths that have been installed
-            # check if they are actually of interest
-            # by matching against the eggs that were specified in the buildout
-
-            # egg paths are of the form Name-Version-PyVersion.
-            # So, check that we are starting with the right name, and finishing in a -
-            # Should give the name of the egg that was installed, so we can match against it
-            self.eggs_of_interest[egg] = [entry_names[x] for x in entry_names.keys() if (x.lower().startswith(egg.lower() + '-') or (x.lower() == egg.lower()))][0]
-
-
-        # now we've done that, set the buildout options we were after
-        for key, value in self.eggs_of_interest.iteritems():
-            self.options.setdefault(key, value)
 
         # get the directory to put the file in
         outputdir = os.path.join(self.buildout['buildout']['parts-directory'], self.name)
@@ -73,7 +67,7 @@ class Eggpaths(object):
         # write out the file
         out_file = open(os.path.join(outputdir, "eggpaths.txt"), 'w')
 
-        for key, value in self.eggs_of_interest.iteritems():
+        for key, value in self.options.iteritems():
             out_file.write(key + ': ' + value + '\n')
 
         # tidy up
