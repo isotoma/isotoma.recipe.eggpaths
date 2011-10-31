284c0c302c78dfd5e276d85ddb1885000e4d8ef6
Alex Holmes <alex@alex-holmes.com>
2011-10-31 12:58:12 +0000

    We do not need a reference file any more (can use omelette)
    Conflicts:
    
    	isotoma/recipe/eggpaths/__init__.py

--- isotoma/recipe/eggpaths/__init__.py
+++ isotoma/recipe/eggpaths/__init__.py
@@ -15,72 +15,36 @@
 import os
 import sys
 import logging
+import pkg_resources
 
-from zc.buildout import easy_install
-from pkg_resources import to_filename
+from zc.buildout import easy_install, UserError
+from zc.recipe.egg import Eggs
 
 class Eggpaths(object):
-    
-    def foo(self, dist):
-        print dist
-        
 
     def __init__(self, buildout, name, options):
-        """ Take the eggs we're given, and save the path to them into buildout variables """ 
-
-        # set up some internal class stuff we'll need later
+        self.eggs = Eggs(buildout, name, options)
         self.buildout = buildout
         self.name = name
         self.options = options
+        self.log = logging.getLogger(__name__)
 
-        # check if we have versions information
-        # if we don't, there's not a lot we can really do
-        if not self.buildout['buildout'].has_key('versions'):
-            # log and bail
-            logging.error('Buildout does not have versions information')
-            sys.exit(1)
-        
-        # get the versions section from buildout
-        versions_name = self.buildout['buildout']['versions']
-        versions_section = self.buildout[versions_name]
-        
-        # stick this in our local buildout space
-        # we also need to add the path to the egg
-        for k,v in versions_section.iteritems():
-            egg_directory = self.buildout['buildout']['eggs-directory']
-            python_version = '.'.join([str(x) for x in sys.version_info[0:2]])
-            k = to_filename(k)
-            v = to_filename(v)
+        egg_list =  [s.strip() for s
+                    in self.options.get('eggs', '').splitlines()
+                    if s.strip()]
 
-            path_to_egg = os.path.join(egg_directory, k + '-' + v + '-py' + python_version +'.egg/')
-            
-            self.options.setdefault(k, path_to_egg)
-        
+        requirements, ws = self.eggs.working_set(egg_list)
+
+        for egg_name in egg_list:
+            requirement = pkg_resources.Requirement.parse(egg_name)
+            package = ws.find(requirement)
+            if package:
+                self.log.debug('%s = %s' % (requirement, package.location))
+                self.options.setdefault(egg_name, package.location)
+            else:
+                raise UserError('Cannot locate the egg path for %s' % egg_name)
 
     def install(self):
-        """ Write out the paths that we've found to a handy text file """
+        return ()
 
-        # get the directory to put the file in
-        outputdir = os.path.join(self.buildout['buildout']['parts-directory'], self.name)
-
-        # create this directory
-        if not os.path.exists(outputdir):
-            os.mkdir(outputdir)
-
-        # write out the file
-        out_file = open(os.path.join(outputdir, "eggpaths.txt"), 'w')
-
-        for key, value in self.options.iteritems():
-            out_file.write(key + ': ' + value + '\n')
-
-        # tidy up
-        out_file.flush()
-        out_file.close()
-
-        # buildout expects paths that have been altered
-        return outputdir
-
-
-
-    def update(self):
-        self.install()
+    update = install
